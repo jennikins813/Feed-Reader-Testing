b20e96d2de1d91b9a3144bc1708056282a21647b
jennikins813 <jennikins813@yahoo.com>
2016-09-09 10:58:24 -0700

    style: Adjust closing brackets accordingly

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -98,14 +98,14 @@ $(function() {
             expect(loadFeed).toBeGreaterThan(0);
             done();
         });
+    });
 
-
-    /* TODO: Write a new test suite named "New Feed Selection"
+    /* TODO: Write a new test suite named "New Feed Selection" */
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-    });
+    
      
 }());
